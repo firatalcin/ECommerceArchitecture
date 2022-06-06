bc29ad85cebcba6d0eebefd0a496734246b68f3e
Fırat Alçın <firatalcin04@gmail.com>
2022-06-06 19:52:29 +0300

    Database Design and Migration

--- ETicaretAPI/Infrastructure/Persistence/ServiceRegistration.cs
+++ ETicaretAPI/Infrastructure/Persistence/ServiceRegistration.cs
@@ -1,5 +1,8 @@
 ﻿using ETicaretAPI.Application.Abstractions;
 using ETicaretAPI.Persistence.Concretes;
+using ETicaretAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -14,6 +17,8 @@ namespace ETicaretAPI.Persistence
         public static void AddPersistenceServices(this IServiceCollection services)
         {
             services.AddSingleton<IProductService, ProductService>();
+
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
         }
     }
 }
