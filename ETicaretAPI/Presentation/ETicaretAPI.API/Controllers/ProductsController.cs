c73802e706caaea2316e4d647d57febc39126670
Fırat Alçın <firatalcin04@gmail.com>
2022-06-09 13:49:41 +0300

    Tracking Performance Optimization

--- ETicaretAPI/Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ ETicaretAPI/Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -22,13 +22,17 @@ namespace ETicaretAPI.API.Controllers
         [HttpGet]
         public async Task Get()
         {
-            await _productWriteRepository.AddRangeAsync(new()
-            {
-                new() {Id = Guid.NewGuid(), Name= "Product 1", Price = 100, CreatedDate = DateTime.UtcNow, Stock = 10},
-                new() {Id = Guid.NewGuid(), Name= "Product 2", Price = 200, CreatedDate = DateTime.UtcNow, Stock = 20},
-                new() {Id = Guid.NewGuid(), Name= "Product 3", Price = 300, CreatedDate = DateTime.UtcNow, Stock = 30}
-            });
-            await _productWriteRepository.SaveAsync();
+            //await _productWriteRepository.AddRangeAsync(new()
+            //{
+            //    new() {Id = Guid.NewGuid(), Name= "Product 1", Price = 100, CreatedDate = DateTime.UtcNow, Stock = 10},
+            //    new() {Id = Guid.NewGuid(), Name= "Product 2", Price = 200, CreatedDate = DateTime.UtcNow, Stock = 20},
+            //    new() {Id = Guid.NewGuid(), Name= "Product 3", Price = 300, CreatedDate = DateTime.UtcNow, Stock = 30}
+            //});
+            //await _productWriteRepository.SaveAsync();
+
+            Product p = await _productReadRepository.GetByIdAsync("b42e4c57-f94e-43cb-b614-4b9e3026ad50", false);
+            p.Name = "Armut";
+            _productWriteRepository.SaveAsync();
         }
 
         [HttpGet("{id}")]
